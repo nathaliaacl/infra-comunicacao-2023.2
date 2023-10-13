4ed802cc1b4e66bc5fd7dea7542ebaa1df112cd3
nathaliaacl <114547964+nathaliaacl@users.noreply.github.com>
2023-10-13 11:49:35 -0300

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,13 @@
+import socket 
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1333))
+
+#controle das mensagens e do tamanho delas
+full_msg = ''
+while True:
+    msg = s.recv(8)
+    if len(msg) <= 0: #enquanto houver texto 
+        break
+    full_msg += msg.decode("utf-8")
+print(full_msg)
