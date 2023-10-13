96dc451800fcb1cea0e970b0158daf32be1e9aeb
Leonardo-Medeiros <lmf@cesar.school>
2023-10-13 17:03:39 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -4,6 +4,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1333))
 
 #controle das mensagens e do tamanho delas
+
 full_msg = ''
 while True:
     msg = s.recv(8)
